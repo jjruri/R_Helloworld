57d8ab249a2a40d4de028dfee513f24679de176b
jjruri <jj.ruri11280506@gmail.com>
2019-12-29 15:42:00 +0900

    Initial Commit

---
+++ R_HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  R_HelloWorld
+//
+//  Created by 佐藤るり on 2019/12/29.
+//  Copyright © 2019 junya.satou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
